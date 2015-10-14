7e31096fc82642264f776e3c01d682b9b33ec9c9
jx <470042560@qq.com>
2015-10-14 13:02:03 +0800

    初始化项目Test

---
+++ GitHubTest/GitHubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubTest
+//
+//  Created by 张盼盼 on 15/10/14.
+//  Copyright © 2015年 huawei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubTest/GitHubTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitHubTest
+//
+//  Created by 张盼盼 on 15/10/14.
+//  Copyright © 2015年 huawei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
